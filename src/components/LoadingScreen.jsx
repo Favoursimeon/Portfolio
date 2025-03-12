2798cf15b9defc88fd2aed64338d37edf0dddf6a
Favoursimeon <favoursimeon12w@gmail.com>
2025-03-12 17:31:09 +0000

    my potfolio

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -3,7 +3,7 @@ import backgroundImg from "/assets/Hello.png";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<Hello Wolrd, This is Favour/>";
+  const fullText = "<Hello World, This is Favour/>";
 
   useEffect(() => {
     let index = 0;
